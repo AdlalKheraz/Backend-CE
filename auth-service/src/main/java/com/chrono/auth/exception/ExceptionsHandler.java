3242692d95ac6fa480d9f571b4194c62c2a43132
seylitanez <lyes.zenati@hotmail.com>
2025-05-17 15:18:04 +0100

    docs: update response format for exceptions

--- auth-service/src/main/java/com/chrono/auth/exception/ExceptionsHandler.java
+++ auth-service/src/main/java/com/chrono/auth/exception/ExceptionsHandler.java
@@ -1,15 +1,35 @@
 package com.chrono.auth.exception;
 
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @ControllerAdvice
 public class ExceptionsHandler {
-  @ExceptionHandler(Exception.class)
-  public ResponseEntity<String> handleException(Exception e) {
-    System.out.println(e.getMessage());
-    return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+  
+
+  @ExceptionHandler(RuntimeException.class)
+  public ResponseEntity<Object> handleRuntimeException(RuntimeException ex) {
+    Map<String, Object> response = new HashMap<>();
+    response.put("timestamp", LocalDateTime.now());
+    response.put("message", ex.getMessage());
+    response.put("status", HttpStatus.BAD_REQUEST.value());
+
+    return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
   }
+
+  @ExceptionHandler(MethodArgumentNotValidException.class)
+  public ResponseEntity<Object> handleValidation(MethodArgumentNotValidException ex) {
+    Map<String, String> errors = new HashMap<>();
+    ex.getBindingResult().getFieldErrors().forEach(error ->
+      errors.put(error.getField(), error.getDefaultMessage())
+    );
+    return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
+  }
+
 }
