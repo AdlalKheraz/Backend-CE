40022c155eed96758da0f34aa064628a43780d10
seylitanez <lyes.zenati@hotmail.com>
2025-05-18 20:10:28 +0100

    feat: add @Repository annotation to CommentRepository

--- event-service/src/main/java/com/chrono/event/repository/CommentRepository.java
+++ event-service/src/main/java/com/chrono/event/repository/CommentRepository.java
@@ -3,9 +3,11 @@ package com.chrono.event.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.chrono.event.entity.Comment;
 
+@Repository
 public interface CommentRepository extends JpaRepository<Comment, Long> {
     List<Comment> findByEventId(Long eventId);
 }
