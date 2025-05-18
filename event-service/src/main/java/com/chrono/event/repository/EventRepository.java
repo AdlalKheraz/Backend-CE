82899356fa39d60001f97d564d7221581c627006
seylitanez <lyes.zenati@hotmail.com>
2025-05-18 20:09:37 +0100

    type: add @Repository annotation to repositories

--- event-service/src/main/java/com/chrono/event/repository/EventRepository.java
+++ event-service/src/main/java/com/chrono/event/repository/EventRepository.java
@@ -3,9 +3,11 @@ package com.chrono.event.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.chrono.event.entity.Event;
 
+@Repository
 public interface EventRepository extends JpaRepository<Event, Long> {
     List<Event> findByCivilizationId(Long civilizationId);
 }
