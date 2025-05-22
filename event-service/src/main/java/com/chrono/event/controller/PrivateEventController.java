9c3209eb4c1343b5358a395f0140ec79b0d324c8
seylitanez <lyes.zenati@hotmail.com>
2025-05-22 22:28:12 +0100

    feat: add pagination and filtering to private event search

--- event-service/src/main/java/com/chrono/event/controller/PrivateEventController.java
+++ event-service/src/main/java/com/chrono/event/controller/PrivateEventController.java
@@ -3,6 +3,7 @@ package com.chrono.event.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,10 +13,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.chrono.event.dto.EventDTO;
+import com.chrono.event.dto.EventFilterDTO;
 import com.chrono.event.dto.PrivateEventDTO;
-import com.chrono.event.entity.Event;
-import com.chrono.event.service.EventService;
 import com.chrono.event.service.PrivateEventService;
 
 @RestController
@@ -30,6 +29,17 @@ public class PrivateEventController {
         return eventService.createEvent(dto);
     }
 
+    /**
+     * Récupère tous les événements avec pagination et filtrage
+     */
+    @PostMapping("/search")
+    public Page<PrivateEventDTO> searchEvents(@RequestBody EventFilterDTO filter) {
+        return eventService.getAllPrivateEvents(filter);
+    }
+
+    /**
+     * Méthode de compatibilité pour récupérer tous les événements sans filtrage
+     */
     @GetMapping
     public List<PrivateEventDTO> getAllPrivateEvents() {
         return eventService.getAllPrivateEvents();
@@ -40,6 +50,19 @@ public class PrivateEventController {
         return eventService.getPrivateEventById(id);
     }
 
+    /**
+     * Récupère les événements d'une civilisation avec pagination et filtrage
+     */
+    @PostMapping("/civilization/{id}/search")
+    public Page<PrivateEventDTO> searchEventsByCivilization(
+            @PathVariable Long id, 
+            @RequestBody EventFilterDTO filter) {
+        return eventService.getPrivateEventsByCivilization(id, filter);
+    }
+
+    /**
+     * Méthode de compatibilité pour récupérer les événements d'une civilisation sans filtrage
+     */
     @GetMapping("/civilization/{id}")
     public List<PrivateEventDTO> getPrivateEventsByCivilization(@PathVariable Long id) {
         return eventService.getPrivateEventsByCivilization(id);
